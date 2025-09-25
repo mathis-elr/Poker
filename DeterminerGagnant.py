eac448c375e4e072ac708bcb208afeb2d6e6d1ad
mathis-elr <mathis.eloire15@gmail.com>
2025-09-25 21:55:11 +0200

    ajout d'une classe determinerMain

--- DeterminerGagnant.py
+++ DeterminerGagnant.py
@@ -1,10 +1,24 @@
 from customtkinter import *
 
+from DeterminerMain import DeterminerMain
+
 class DeterminerGagnant():
     
     def __init__(self, partie):
         self.partie = partie
         
-        self.labelGagant= CTkLabel(self.partie.frameCartes, font=("Arial",15),text_color="orange", text="fin de manche")
-        self.labelGagant.grid(row=1,column=1)
+        self.mains = {}
+        self.gagant = {"joueur" : None,"main" : None}
         
+        for joueur in self.partie.manche.liste_joueurs:
+            main = DeterminerMain(joueur)
+            self.mains[joueur] = main
+        
+        self.comparerMains()
+        
+        self.labelGagant= CTkLabel(self.partie.frameCartes, font=("Arial",15),text_color="orange", text="{} gagne avec {}".format(self.comparerMains["joueur"], self.comparerMains["main"]))
+        self.labelGagant.grid(row=1,column=1,columnspan=5)
+    
+    
+    def comparerMains(self):
+        pass
